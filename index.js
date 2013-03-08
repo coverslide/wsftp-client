c18140c197fd7fff7cfc29db7a7537b0b910e564
Richard Hoffman <coverslide@gmail.com>
2013-03-08 12:17:03 -0800

    Emit proper Events, use callback

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
+'use strict'
+
 module.exports = WSFTPClient
 
 require('mkee')(WSFTPClient)
 require('mkstream')(WSFTPClientRequest)
+var Buffer = require('buffer').Buffer
 
 function WSFTPClient(url, options){
   var _this = Object.create(WSFTPClient.prototype)
   _this.url = url
   _this.options = options
   _this.requestId = 0
-  _this.requests = []
   return _this
 }
 
@@ -22,31 +24,15 @@ WSFTPClient.prototype.request = function request(url, options, cb){
   var self = this
   var ws = new WebSocket(this.url)
   var request = new WSFTPClientRequest(ws, url, options) 
-  var id = this.requestId++
-
-  //request.id = id
-  //request.client = this
-
-  this.requests[id] = request
 
   if(cb){
     request.once('stat', function(stat){
-      if(stat.directory){
-        request.once('directoryStats', function(directoryStats){
-          cb(null, stat, directoryStats)
-        })
-      } else {
-        cb(null, stat, request)
-      }
+      cb(null, stat, request)
     })
   }
 
-  request.on('end', function(){
-    self.requests[id] = null
-  })
-
   request.on('error', function(error){
-    self.requests[id] = null
+    if(cb) cb(error)
   })
 
   return request
@@ -54,12 +40,11 @@ WSFTPClient.prototype.request = function request(url, options, cb){
 
 function WSFTPClientRequest(ws, url, options){
   var _this = Object.create(WSFTPClientRequest.prototype)
+  var ended = false
 
   _this.ws = ws
   _this.options = options
   _this.readable = true
-
-  var firstResponseRecieved = false
 
   ws.binaryType = 'arraybuffer'
   ws.addEventListener('message', onMessage, false)
@@ -84,20 +69,29 @@ function WSFTPClientRequest(ws, url, options){
       } catch(e){
         return _this.emit('error', e)
       }
-      if(data.error)
-        _this.emit('error', e)
-      else if(data.stat)
+      if(data.error){
+        _this.emit('error', new Error(data.error))
+      }else if(data.filename){
+        _this.emit('filestat', data)
+      }else if(data.stat){
         _this.emit('stat', data.stat)
-      else if(data.directoryStats)
-        _this.emit('directoryStats', data.directoryStats)
-    } else if(data.constructor.name == "ArrayBuffer"){
+      }else if (data.end){
+        _this.emit('end')
+        ended = true    
+      }
+    } else if(data instanceof Buffer){
       _this.emit('data', data)
+    } else if(data instanceof ArrayBuffer){
+      _this.emit('data', new Buffer(new Uint8Array(data)))
     } else {
       _this.emit('error', new Error('Unknown Data Type'))
     }
   }
 
   function onClose(e){
-    _this.emit('end')
+    if(!ended){
+      _this.emit('error', new Error('Socket closed prematurely'))
+    }
+    _this.emit('close', e)
   }
 }
